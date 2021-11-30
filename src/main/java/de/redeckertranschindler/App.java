e87cbd51013d27968e6eb825773eb9c603bad7b6
iowi479 <simon@alle-schindlers.de>
2021-11-30 17:39:35 +0100

    updated Graph-class

--- src/main/java/de/redeckertranschindler/App.java
+++ src/main/java/de/redeckertranschindler/App.java
@@ -1,11 +1,20 @@
 package de.redeckertranschindler;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileNotFoundException;
+
 public class App {
     public static void main(String[] args) {
-        new Graph();
+        try {
+
+            // new Graph("E:\\Programmierprojekt\\germany.fmi"); // Danny
+            // new Graph("E:\\Programmierprojekt\\germany.fmi"); // Mickey
+            Graph g = new Graph("E:\\Programmierprojekt\\germany.fmi"); // Simon
+
+            System.out.println(g.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
--- src/main/java/de/redeckertranschindler/Graph.java
+++ src/main/java/de/redeckertranschindler/Graph.java
@@ -5,72 +5,122 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Graph {
-    public Graph() {
 
-        long time = System.nanoTime();
+    /** Coordinatelist index */
+    public static final int X = 0;
+    /** Coordinatelist index */
+    public static final int Y = 1;
 
-        int n = 0;
-        int m = 0;
+    /** Adjacencylist index */
+    public static final int SRCNODE = 0;
+    /** Adjacencylist index */
+    public static final int TARGETNODE = 1;
+    /** Adjacencylist index */
+    public static final int WEIGHT = 2;
 
-        double[][] coordinates = new double[0][0];
-        int[] offset = new int[0];
-        int[][] adj = new int[0][0];
+    private final String graphFilePath;
 
+    /**
+     * Number of Nodes
+     */
+    private final int n;
+    /**
+     * Number of Edges
+     */
+    private final int m;
+
+    /**
+     * Coordinates of all the Nodes
+     * 
+     * First dimension: Coordinateaxis
+     * Second dimension: Node-Id
+     */
+    private final double[][] coordinates;
+
+    /**
+     * An offset for each Node to know where to iterate through the adjacencylist
+     */
+    private final int[] offset;
+
+    /**
+     * Adjacencylist of all Edges
+     * 
+     * First dimension: information
+     * Second dimension: Node-Id
+     */
+    private final int[][] adjacencyList;
+
+    /**
+     * Generates a Graph ready to use!
+     * 
+     * @param graphFilePath path to the specificly formated .fmi file
+     * @throws FileNotFoundException
+     */
+    public Graph(final String graphFilePath) throws FileNotFoundException {
+
+        this.graphFilePath = graphFilePath;
+        File graphFile = new File(graphFilePath);
+        Scanner graphFileReader;
         try {
-            File myObj = new File("P:\\InputFileTest\\germany.fmi");
-            Scanner myReader = new Scanner(myObj);
-            for (int i = 0; i < 5; i++) {
-                myReader.nextLine();
-            }
-
-            n = Integer.parseInt(myReader.nextLine());
-            m = Integer.parseInt(myReader.nextLine());
-
-            coordinates = new double[2][n];
-            offset = new int[n];
-            adj = new int[3][m];
-
-            System.out.println("nodes " + n);
-            System.out.println("edges " + m);
-
-            for (int i = 0; i < n; i++) {
-                String data = myReader.nextLine();
-                String[] parts = data.split(" ");
-
-                int id = Integer.parseInt(parts[0]);
-                coordinates[0][id] = Double.parseDouble(parts[2]);
-                coordinates[1][id] = Double.parseDouble(parts[3]);
-
-            }
-
-            int src = 0;
-            int counter = 0;
-
-            for (int i = 0; i < m; i++) {
-                String data = myReader.nextLine();
-                String[] parts = data.split(" ");
-                int currentSrc = Integer.parseInt(parts[0]);
-                if (currentSrc != src) {
-                    offset[src] = counter;
-
-                    src = currentSrc;
-                    counter = 0;
-                }
-
-                counter++;
-
-                adj[0][i] = Integer.parseInt(parts[0]);
-                adj[1][i] = Integer.parseInt(parts[1]);
-                adj[2][i] = Integer.parseInt(parts[2]);
-            }
-
-            myReader.close();
+            graphFileReader = new Scanner(graphFile);
         } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            throw new FileNotFoundException("Die angegebene Graphdatei konnte nicht geöffnet werden!");
         }
 
-        System.out.println(((System.nanoTime() - time) / 1000000000) + " Sek.");
+        // We don't care about the first 5 lines
+        for (int i = 0; i < 5; i++) {
+            graphFileReader.nextLine();
+        }
 
+        n = Integer.parseInt(graphFileReader.nextLine());
+        m = Integer.parseInt(graphFileReader.nextLine());
+
+        coordinates = new double[2][n];
+        offset = new int[n];
+        adjacencyList = new int[3][m];
+
+        for (int i = 0; i < n; i++) {
+            String data = graphFileReader.nextLine();
+            String[] parts = data.split(" ");
+
+            int id = Integer.parseInt(parts[0]);
+            coordinates[X][id] = Double.parseDouble(parts[2]);
+            coordinates[Y][id] = Double.parseDouble(parts[3]);
+
+        }
+
+        int src = 0;
+        int counter = 0;
+
+        for (int i = 0; i < m; i++) {
+            String data = graphFileReader.nextLine();
+            String[] parts = data.split(" ");
+            int currentSrc = Integer.parseInt(parts[0]);
+            if (currentSrc != src) {
+                offset[src] = counter;
+
+                src = currentSrc;
+                counter = 0;
+            }
+
+            counter++;
+
+            adjacencyList[SRCNODE][i] = Integer.parseInt(parts[0]);
+            adjacencyList[TARGETNODE][i] = Integer.parseInt(parts[1]);
+            adjacencyList[WEIGHT][i] = Integer.parseInt(parts[2]);
+        }
+
+        graphFileReader.close();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                    Graph (File: \"%s\")
+                        Nodes: %d
+                        Edges: %d
+                """,
+                graphFilePath,
+                n, m);
     }
 }
