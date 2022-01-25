637e3e9497d2b777779360c071b28aa711ad0088
iowi479 <simon@alle-schindlers.de>
2022-01-25 10:44:13 +0100

    clean up

--- src/main/java/de/redeckertranschindler/Graph.java
+++ src/main/java/de/redeckertranschindler/Graph.java
@@ -150,86 +150,15 @@ public class Graph {
                 graphFilePath, n, m);
     }
 
-    // /**
-    // * calculates the distance between two nodes via Dijkstra algorithm
-    // *
-    // * @param startId coordinates of the start node
-    // * @param endId coordinates of the target node
-    // * @return shortest distance from starting node to target node
-    // */
-    // public int oneToOneDijkstra(final int startId, final int endId) {
-
-    // final int[] distances = new int[n];
-    // final int[] previousNode = new int[n];
-    // final boolean[] finished = new boolean[n];
-
-    // final Queue<Integer> priorityQueue = new PriorityQueue<>(n, new
-    // Comparator<Integer>() {
-    // @Override
-    // public int compare(final Integer node1, final Integer node2) {
-    // return distances[node1] - distances[node2];
-    // }
-    // });
-
-    // previousNode[startId] = startId;
-    // for (int i = 0; i < n; i++) {
-    // distances[i] = Integer.MAX_VALUE;
-    // }
-    // distances[startId] = 0;
-    // priorityQueue.add(startId);
-
-    // while (!priorityQueue.isEmpty()) {
-
-    // final int srcNode = priorityQueue.poll();
-
-    // // Difference to OneToAll
-    // if (endId >= 0 && srcNode == endId) {
-    // return distances[endId];
-    // }
-    // // ----------------------
-    // // bereits bearbeitete Knoten
-    // if (!finished[srcNode]) {
-
-    // finished[srcNode] = true;
-
-    // final int startOfEdges = offset[srcNode];
-    // final int endOfEdges = srcNode == n - 1 ? m : offset[srcNode + 1];
-
-    // for (int i = startOfEdges; i < endOfEdges; i++) {
-    // final int weight = adjacencyList[WEIGHT][i];
-    // final int targetNode = adjacencyList[TARGETNODE][i];
-
-    // if (distances[targetNode] > distances[srcNode] + weight) {
-    // distances[targetNode] = distances[srcNode] + weight;
-    // previousNode[targetNode] = srcNode;
-
-    // priorityQueue.add(targetNode);
-    // continue;
-    // }
-
-    // if (!finished[targetNode]) {
-    // priorityQueue.add(targetNode);
-    // }
-
-    // }
-
-    // }
-    // }
-
-    // // should never happen...
-    // return Integer.MAX_VALUE;
-    // }
-
     public int[] dijkstra(final int startId) {
         return dijkstra(startId, -1);
     }
 
     /**
-     * Given a positively weighted graph and a starting node, Dijkstra determines
-     * the shortest path and distance
-     * from the source to all destinations in the graph
+     * dijkstra implementation
      * 
      * @param startId
+     * @param endId
      * @return shortest distances from starting node to all nodes in graph
      */
     public int[] dijkstra(final int startId, final int endId) {
